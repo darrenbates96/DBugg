e7d288ea4381f08c35eed4989403c392bb3598cf
Darren <darren@specno.com>
2021-07-17 12:38:01 +0200

    Instantiate type safe component variables for birds-eye-view-page and problem-page components

--- src/app/cosmetic/problem/pages/problem-page/problem-page.component.ts
+++ src/app/cosmetic/problem/pages/problem-page/problem-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Problem } from 'src/app/core/models';
 
 @Component({
   selector: 'app-problem-page',
   templateUrl: './problem-page.component.html',
-  styleUrls: ['./problem-page.component.scss']
+  styleUrls: ['./problem-page.component.scss'],
 })
 export class ProblemPageComponent implements OnInit {
+  problem: Problem = {} as Problem;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

e388fd0fccadbabf1576acf7219f4826751febab
Darren <darren@specno.com>
2021-07-17 11:12:10 +0200

    Add contact page, update panel routing to include new contact route

--- src/app/cosmetic/panel/panel.module.ts
+++ src/app/cosmetic/panel/panel.module.ts
@@ -5,9 +5,10 @@ import { PanelRoutingModule } from './panel-routing.module';
 import { PanelComponent } from './panel.component';
 import { BirdsEyeViewPageComponent } from './pages/birds-eye-view-page/birds-eye-view-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [PanelComponent, BirdsEyeViewPageComponent, AboutPageComponent],
+  declarations: [PanelComponent, BirdsEyeViewPageComponent, AboutPageComponent, ContactPageComponent],
   imports: [CommonModule, PanelRoutingModule],
 })
 export class PanelModule {}

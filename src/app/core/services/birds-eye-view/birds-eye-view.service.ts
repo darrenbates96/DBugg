224763f7e8de7a1fef1496744244b47cdd9534df
Darren Bates <darren@specno.com>
2021-08-01 21:50:10 +0200

    Set up birds eye view service for reading in birds eye view problems

--- src/app/core/services/birds-eye-view/birds-eye-view.service.ts
+++ src/app/core/services/birds-eye-view/birds-eye-view.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { BirdsEyeViewProblem } from 'src/app/core/models';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class BirdsEyeViewService {
-  constructor() {}
+	constructor(private angularFireStore: AngularFirestore) {}
 
-  getBirdsEyeViewProblems(): Observable<BirdsEyeViewProblem[]> {}
+	getBirdsEyeViewProblems() {
+		return this.angularFireStore
+			.collection<BirdsEyeViewProblem>('birdseyeviewproblems')
+			.valueChanges();
+	}
 }
--- src/app/cosmetic/panel/pages/birds-eye-view-page/birds-eye-view-page.component.ts
+++ src/app/cosmetic/panel/pages/birds-eye-view-page/birds-eye-view-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { BirdsEyeViewProblem } from 'src/app/core/models';
+import { BirdsEyeViewService } from 'src/app/core/services/birds-eye-view/birds-eye-view.service';
 
 @Component({
-  selector: 'app-birds-eye-view-page',
-  templateUrl: './birds-eye-view-page.component.html',
-  styleUrls: ['./birds-eye-view-page.component.scss'],
+	selector: 'app-birds-eye-view-page',
+	templateUrl: './birds-eye-view-page.component.html',
+	styleUrls: ['./birds-eye-view-page.component.scss'],
 })
 export class BirdsEyeViewPageComponent implements OnInit {
-  birdsEyeViewProblems: BirdsEyeViewProblem[] = [];
+	birdsEyeViewProblems: BirdsEyeViewProblem[] = [];
 
-  constructor() {}
+	closeSubscriptions: Subject<void> = new Subject<void>();
 
-  ngOnInit(): void {}
+	constructor(private birdsEyeViewService: BirdsEyeViewService) {}
+
+	ngOnInit(): void {
+		this.birdsEyeViewService
+			.getBirdsEyeViewProblems()
+			.pipe(takeUntil(this.closeSubscriptions))
+			.subscribe((birdsEyeViewProblems) => {
+				if (birdsEyeViewProblems.length) {
+					this.birdsEyeViewProblems = birdsEyeViewProblems;
+				}
+			});
+	}
+
+	ngOnDestroy(): void {
+		this.closeSubscriptions.next();
+	}
 }

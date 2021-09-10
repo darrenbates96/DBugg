397bb4f3d89da2cc05124a344c2f23e9a25b4c51
Darren <darren@specno.com>
2021-09-10 08:01:13 +0200

    implement first create-problem input, create global input styles, extract content styles to global

--- src/app/cosmetic/problem/problem.module.ts
+++ src/app/cosmetic/problem/problem.module.ts
@@ -1,13 +1,22 @@
+// Module Imports
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ProblemRoutingModule } from './problem-routing.module';
+
+// Component Imports
 
 import { ProblemComponent } from './problem.component';
 import { ProblemPageComponent } from './pages/problem-page/problem-page.component';
 import { CreateProblemPageComponent } from './pages/create-problem-page/create-problem-page.component';
 
 @NgModule({
-  declarations: [ProblemComponent, ProblemPageComponent, CreateProblemPageComponent],
-  imports: [CommonModule, ProblemRoutingModule],
+	declarations: [
+		ProblemComponent,
+		ProblemPageComponent,
+		CreateProblemPageComponent,
+	],
+	imports: [CommonModule, FormsModule, ProblemRoutingModule],
 })
 export class ProblemModule {}
